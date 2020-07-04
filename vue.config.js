132c62b6c46fb99f4ad7aca1fe49aac1377eb17e
sixd <nakazatokenya@gmail.comm>
2020-07-04 18:13:55 +0900

    #1 ローカル実行用とWebアプリ用のパスを切った

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, '/app/src'), // 1. @の参照先の変更
+      },
+    },
+  },
+  outputDir: 'app/src/public', // 2. 出力先
+  pages: {
+    index: {
+      entry: 'app/src/main.js', // エントリーポイント
+      template: 'app/public/index.html', //3. index.htmlテンプレート
+      filename: 'index.html', // 省略可
+    },
+  },
+}
